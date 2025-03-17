e624d58de02975e190ddb1883edb6d0d635dac9b
Marcin Baranek <baranekmarcin47@gmail.com>
2025-03-17 09:27:22 +0100

    Fix test number 5

--- tests/test_task_5.py
+++ tests/test_task_5.py
@@ -6,9 +6,9 @@ from task_5 import sort_by_value
 @pytest.mark.parametrize(
     "dictionary,expected",
     [
-        ({"one": 1, "two": 2}, [1, 2]),
-        ({"A": 12, "B": 13, "C": -1}, [-1, 12, 13]),
-        ({"A": 1, "B": 2, "C": 13}, [1, 2, 13]),
+        ({"one": 1, "two": 2}, ["one", "two"]),
+        ({"A": 12, "B": 13, "C": -1}, ["C", "A", "B"]),
+        ({"A": 1, "B": 2, "C": 13}, ["A", "B", "C"]),
     ]
 )
 def test_filter_prime_numbers(dictionary, expected):
